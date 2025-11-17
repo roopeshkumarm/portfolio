4c852f08cf5d929367c5e2cb85660e1f42b32914
Roopesh Kumar <roopeshkumar@Roopesh-MacBook-Pro.local>
2025-11-17 10:12:19 +0530

    remove loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
@@ -7,32 +7,16 @@ import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
-import Loading from './/components/Loading';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate a loading time
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  }, []);
-
   return (
     <div className="App">
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <>
-          <NavBar />
-          <Banner />
-          <Skills />
-          <Projects />
-          <Contact />
-          <Footer />
-        </>
-      )}
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
